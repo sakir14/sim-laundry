a88744e93213ac4e377fbd0ec19e1f03a2cf1c57
sakir14 <sakirarfin547@gmail.com>
2026-05-28 10:58:14 +0700

    Update app branding title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'SIM-LAUNDRY') }}</title>
+        <title>{{ config('app.name', 'AWAN-LAUNDRY') }} | Login</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800,900&display=swap" rel="stylesheet" />
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
